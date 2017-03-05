d9523de61cfd4a8d6234548162fac83a87c5c5b6
Joshua Bradford <jjyet@yahoo.com>
2017-03-04 19:15:28 -0500

    added basic board to template

--- src/js/view/template.js
+++ src/js/view/template.js
@@ -5,7 +5,9 @@ function writeTest(){
   var prime = new Scoreboard(1, 5);
   $('.container').html(`
     <div class='title'>Welcome to Whack-a-Bug!!!</div>
+    <div><img class='board' src='./images/mole_background.jpeg'/></div>
     <div class='instructions'>${prime.writeInstructions()}</div>
+
     `);
 }
 
