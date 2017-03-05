19f99c72c7a6e8557550ffe2922bf52897bde974
Joshua Bradford <jjyet@yahoo.com>
2017-03-05 14:15:55 -0500

    added boss picture realigned instructions and where the bugs pop

--- src/js/view/template.js
+++ src/js/view/template.js
@@ -5,8 +5,9 @@ function writeTest(object){
   //var prime = new Scoreboard(1, 5);
   $('.container').html(`
     <div class='title'>Welcome to Whack-a-Bug!!!</div>
+    <img id ='boss' src='./images/boss1.png'/>
+    <div class='instructions'><br><br><br>${object.writeInstructions()}</div>
     <div><img id='board' src='./images/mole_background.jpeg'/></div>
-    <div class='instructions'>${object.writeInstructions()}</div>
     <div id='hole-1' class='height0'></div>
     <div id='hole-2' class='height0'></div>
     <div id='hole-3' class='height0'></div>
