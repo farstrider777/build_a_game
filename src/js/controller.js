d470d49f8b6228f6f8da89747c39d32f1811d525
Joshua Bradford <jjyet@yahoo.com>
2017-03-05 01:21:19 -0500

    switched to bottom justified abosolute position for proper css transition effect, tested effect on top left hole. adding a set of 5 random bugs to appear for testing purposes

--- src/js/controller.js
+++ src/js/controller.js
@@ -13,7 +13,38 @@ for (var i = 0; i < startBugs; i++) {
   currentBugs.push(main.generateBug(1));
 }
 
-$('#hole-2').html(currentBugs[0].name);
+var holeChoice = 1 + Math.floor((Math.random() * 10));
+$(`#hole-${holeChoice}`).html(currentBugs[0].name);
+$(`#hole-${holeChoice}`).removeClass('height0');
+$(`#hole-${holeChoice}`).addClass('height60');
+
+holeChoice = 1 + Math.floor((Math.random() * 10));
+$(`#hole-${holeChoice}`).html(currentBugs[1].name);
+$(`#hole-${holeChoice}`).removeClass('height0');
+$(`#hole-${holeChoice}`).addClass('height60');
+
+holeChoice = 1 + Math.floor((Math.random() * 10));
+$(`#hole-${holeChoice}`).html(currentBugs[2].name);
+$(`#hole-${holeChoice}`).removeClass('height0');
+$(`#hole-${holeChoice}`).addClass('height60');
+
+holeChoice = 1 + Math.floor((Math.random() * 10));
+$(`#hole-${holeChoice}`).html(currentBugs[3].name);
+$(`#hole-${holeChoice}`).toggleClass('height0');
+$(`#hole-${holeChoice}`).toggleClass('height60');
+
+holeChoice = 1 + Math.floor((Math.random() * 10));
+$(`#hole-${holeChoice}`).html(currentBugs[4].name);
+$(`#hole-${holeChoice}`).toggleClass('height0');
+$(`#hole-${holeChoice}`).toggleClass('height60');
+
+function killBug(event){
+  console.log(event.target);
+  $(event.target).toggleClass('height0');
+  $(event.target).toggleClass('height60');
+}
+
+$('#hole-1').click(killBug);
 
 
 export { prime, currentBugs };
--- src/js/view/template.js
+++ src/js/view/template.js
@@ -7,16 +7,16 @@ function writeTest(object){
     <div class='title'>Welcome to Whack-a-Bug!!!</div>
     <div><img class='board' src='./images/mole_background.jpeg'/></div>
     <div class='instructions'>${object.writeInstructions()}</div>
-    <div id='hole-1'></div>
-    <div id='hole-2'></div>
-    <div id='hole-3'></div>
-    <div id='hole-4'></div>
-    <div id='hole-5'></div>
-    <div id='hole-6'></div>
-    <div id='hole-7'></div>
-    <div id='hole-8'></div>
-    <div id='hole-9'></div>
-    <div id='hole-10'></div>
+    <div id='hole-1' class='height0'></div>
+    <div id='hole-2' class='height0'></div>
+    <div id='hole-3' class='height0'></div>
+    <div id='hole-4' class='height0'></div>
+    <div id='hole-5' class='height0'></div>
+    <div id='hole-6' class='height0'></div>
+    <div id='hole-7' class='height0'></div>
+    <div id='hole-8' class='height0'></div>
+    <div id='hole-9' class='height0'></div>
+    <div id='hole-10' class='height0'></div>
     `);
 }
 
