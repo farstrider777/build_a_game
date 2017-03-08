e21ca076bd4f6e5c020a532c8432365cad175be3
Joshua Bradford <jjyet@yahoo.com>
2017-03-08 14:27:05 -0500

    added note document for planning future and better obj oriented, added count to generate bug call to get all bugs with unique ids

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,7 +10,7 @@ var prime = new Scoreboard(startLevel, startBugs);
 writeTest(prime);
 var currentBugs = [];
 for (var i = 0; i < startBugs; i++) {
-  currentBugs.push(main.generateBug(1));
+  currentBugs.push(main.generateBug(i));
 }
 
 var numberOfCycles = -1;
@@ -50,7 +50,7 @@ function popBug () {
       prime.bugsToSquash = startBugs;
       writeTest(prime);
       for (var i = 0; i < startBugs; i++) {
-        currentBugs.push(main.generateBug(1));
+        currentBugs.push(main.generateBug(i));
       }
     }else{
       clearInterval(endValue);
---
+++ src/js/notes.js
@@ -0,0 +1,22 @@
+Enclose the whole page. no use of whitespace
+Better board graphic with no artifacts
+Make board from css rules?
+New boss no copyright.
+Make different boards for different levels
+Make playable on mobile, mobile first.
+Better level gradient test with others.
+mouse vs no mouse? Ask user have toggle?
+add bug colors? Add different explosions?
+
+
+Move startBugs and startLevel into prime and use prime to access these
+characteristics
+move currentBugs into board class (add to constructor)?
+Put killBug in new hammer class.
+Rework ifs and put in loop or some structure? readablity?
+
+Question about main why have anything there at all?
+Is my conception of the order the computer runs things correct?
+Should we look into old code?
+my dead addition to the bug class was almost working...
+why was that not working .. was that a good way to model the program?
